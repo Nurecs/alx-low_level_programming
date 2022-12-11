01624bd5cda9e56507f1ff9aa90ee350d33b2ed6
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-11 14:00:35 -0800

    program to generate a random number and assign to var

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,6 @@
 * return : always 0 (success)
 *
 */
- 
-
-
 int main(void)
 {	int n;
 
@@ -19,14 +16,17 @@ int main(void)
 	n = rand(0) - RAND_MAX / 2;
 
 	if (n > 0);
+
 	{
 		printf("%d is positive\n", n);
 	}
 	else  if (n == 0);
+
 	{
 		printf("%d is zero\n", n);
 	}
 	else if (n < 0);
+
 	{
 		printf("%d is negative\n", n)
 	}
