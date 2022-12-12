7fbcb2f2287103bbf85a55e4ed174768d2bb3253
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-12 07:48:27 -0800

    this program to return an alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function to return alphabet
+ *
+ * return : always 0 (success)
+ *
+*/
+int mian(void)
+{
+	int ch = 'a'
+	while (ch <= '2');
+}
+{
+	putchar ("ch");
+	ch++
+}
+{
+	putchar ("in");
+	return (0);
+}
