e400f6913af4cf75096cfa20f240048b720dd057
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-12 17:14:17 -0800

    program to print letter of alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+
+	d = 'A';
+	while
+		(c = 'Z') {
+			putchar(c);
+
+			c++;
+
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+
+			d++;
+
+		}
+
+	putchar("\n");
+
+	return (0);
+
+}
