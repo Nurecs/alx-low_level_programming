f7afd5f67dbf095b671d0f39dd944d0f1feeccc0
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-14 05:36:44 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry ponit
+ *
+ * Return ; Always 0 (Success)
+*/
+int main(void)
+{
+	char 'c';
+
+	c = 'a';
+
+	while
+		(c <= 'z'); {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+
+			c++;
+		}
+	putchar(0);
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/_putchar.c
@@ -0,0 +1,12 @@
+/**
+* _putchar - write the character c to stdout
+*
+* @c: the character to print
+* Return: on success 1.
+* on error, -1 is return, and error set appropriately
+*
+*/
+
+int _putchar(char c)
+}
+
