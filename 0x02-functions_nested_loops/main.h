8b5af7b421443fbd93bdf4789c8b0e953a3de12a
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-16 08:22:15 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _isalpha(int c);
 int _print_sign(int n);
 int _asb(int);
 int print_last_digit(int);
-void jack_bauer(void)
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
