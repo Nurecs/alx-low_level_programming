74e2e57a3230dbec362b90db31a3f1c57d518bc4
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-21 02:35:09 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,22 +14,21 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 		(month >= 3 && day >= 60)
+			day++;
 	}
 	{
-	day++;
+		printf("Day of the year: %d\n", day);
+		printf("remaining days: %d\n", 366 - day);
 	}
-	printf("Day of the year: %d\n", day);
-	printf("remaining days: %d\n", 366 - day);
 	else
 	{
-	if (month == 2 && day == 60)
+		if (month == 2 && day == 60)
 	{
-	printf("invalid date: %02/%02d/%40\n", month, day - 31, year);
+		printf("invalid date: %02/%02d/%40\n", month, day - 31, year);
 	}
 	else
-	}
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d", 365 - day);
-	}
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d", 365 - day);
 	}
 }
