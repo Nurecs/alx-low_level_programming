2a3ab333cf7ab52f364788d7a3d03573b46d5ec2
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-21 23:42:23 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,38 @@
-#include "main.h"
+
+	while (src[j] != '\0' && j < n)
+	{#include "holberton.h"
 
 /**
- * strcat - concatenates the string pointed to by @src, including the terminat
- * null byte, to the end of the string pointed to by @dest.
- * @dest: A poiter to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- * Return: A poiter to the destination string @dest.
+ * _strncat - concatinate the values of two integers.
+ *
+ * @dest: arg a
+ *
+ * @src: arg b
+ *
+ * @n: arg n
+ *
+ * Return: void.
  */
-char *strcat(char *dest, const char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+		dest[i] = src[j];
+		i++;
+		j++;
+
+	}
+
 
 	return (dest);
 }
+
