6167fc1e58cc662a373ee2f201052e06977788de
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-26 20:39:43 -0800

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_triangle - function that prints a triangl
+ * @size: size of the triangle
+ * return: triangle of '#'s
+ */
+
+void print_triagle(int size)
+{
+int sp, ro, tr;
+if (size <= 0)
+{
+putchar('\n');
+}
+
+else
+{
+for (ro = 0; ro <= (size - 1); ro++)
+{
+for (sp = 0; sp < (size - 1) - ro; sp++)
+{
+putchar(' ');
+}
+for (tr = 0; tr <= ro; tr++)
+{
+putchar('#');
+}
+}
+}
+}
