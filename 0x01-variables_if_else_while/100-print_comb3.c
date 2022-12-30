6631a2c8769482297e543f6fb7e94b752ef049af
Nurudeen Abdullahi <ababbatura@gmail.com>
2022-12-29 22:22:00 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
 int main(void)
+
 {
-	int ones = '0';
-	int tens = '0';
+	int d, p;
 
-	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
-
+	for (d = '0'; d < '9'; d++)
 	{
-	for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
-		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repititions*/
-			{
-				putchar(tens);
-
-				Putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
-				{
-					putchar(',');
-
-					putchar(' ');
-				}
-			}
-		}
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
