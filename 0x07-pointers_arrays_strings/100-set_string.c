b5c312cca75a7927fdf8cd5e6b16472e3d3d6693
Nurudeen Abdullahi <ababbatura@gmail.com>
2023-01-02 15:41:30 -0800

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a poiter to a char
+ * @s: poiter to pointer
+ * @to: poiter char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
